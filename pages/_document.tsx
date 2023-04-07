7a4adfa89fac8db4f6165a55235331e1f6760d8b
Rohan Salunkhe <73811790+amrohan@users.noreply.github.com>
2023-04-07 20:26:45 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Creative Studio Collection."
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="https://creative-studio2712.netlify.app/" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Creative Studio Collection."
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="Creative Studio" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta name="twitter:title" content="Creative Studio Collection." />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Creative Studio Collection"
           />
         </Head>
         <body className="bg-black antialiased">
